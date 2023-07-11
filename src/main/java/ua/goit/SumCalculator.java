75339f8ff0e379b4c845da3b66e2defc746b4753
AcceleratorMX <redfield242@gmail.com>
2023-07-12 00:24:10 +0300

    CI/CD

---
+++ src/main/java/ua/goit/SumCalculator.java
@@ -0,0 +1,15 @@
+package ua.goit;
+
+public class SumCalculator {
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+        public static int sum (int n){
+            return switch (n) {
+                case 0 -> throw new IllegalArgumentException();
+                default -> (n * (n + 1)) / 2;
+            };
+        }
+}
+
+
